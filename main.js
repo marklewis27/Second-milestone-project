ed68720e891f88bc53f6732a55771f0ef6cc2f75
marklewis27 <54063883+marklewis27@users.noreply.github.com>
2019-12-03 12:14:54 +0000

    Javascript
    Adding in the first two javascript examples in the grid on the landing page. both fully working ready to move on to the next ones now

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+const textColor = document.querySelectorAll('#scroll-screen-1')
+const listColor = document.querySelectorAll('#example-list li')
+
+
+
 /* Script that is running the loading animation on the index.html page as you first open the site*/
 function myFunction() {
     myVar = setTimeout(showPage, 3000);
@@ -8,4 +13,18 @@ function myFunction() {
     document.getElementById("loading").style.display = "block";
   }
 
+
+  for (text of textColor){
+    text.addEventListener("click", function() {
+      this.style.color = "Yellow";
+      this.style.fontSize = "25px";
+    });
+  }
+
+
+  for (list of listColor){
+    list.addEventListener('click', function(){
+      this.classList.toggle('list-change');
+    });
+  }
 
